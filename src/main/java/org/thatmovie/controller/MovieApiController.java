5dda0c4682b8ffb6ff5b14b68913924a89265186
alexgg9 <alexgalvezgarciaqt@gmail.com>
2024-01-22 12:52:41 +0100

    MovieApiController/Service

--- src/main/java/org/thatmovie/controller/MovieApiController.java
+++ src/main/java/org/thatmovie/controller/MovieApiController.java
@@ -1,7 +1,29 @@
 package org.thatmovie.controller;
 
 
+import com.google.gson.JsonArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.thatmovie.service.MoviesApiService;
 
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/movies")
 public class MovieApiController {
-    //aparece
+    private final MoviesApiService moviesApiService;
+    @Autowired
+    public MovieApiController(MoviesApiService moviesApiService) {
+        this.moviesApiService = moviesApiService;
+    }
+
+    @GetMapping("/list")
+    public JsonArray getMovieList(@RequestParam("page") int page) throws IOException {
+        return moviesApiService.getMovieList(page);
+
+    }
+
 }
--- src/main/java/org/thatmovie/service/MoviesApiService.java
+++ src/main/java/org/thatmovie/service/MoviesApiService.java
@@ -1,5 +1,26 @@
 package org.thatmovie.service;
 
+import com.google.gson.JsonArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thatmovie.repository.MoviesApiRepository;
+
+import java.io.IOException;
+@Service
 public class MoviesApiService {
-    //aparece
+
+    private final MoviesApiRepository moviesApiRepository;
+
+    @Autowired
+    public MoviesApiService(MoviesApiRepository moviesApiRepository) {
+        this.moviesApiRepository = moviesApiRepository;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.moviesApiRepository.setAuthToken(apiKey);
+    }
+
+    public JsonArray getMovieList(int page) throws IOException {
+        return moviesApiRepository.getMoviesList(page);
+    }
 }
