b9bc4bc75a90e3d983de01d319b2712a16d714d1
alexgg9 <alexgalvezgarciaqt@gmail.com>
2024-03-05 09:14:46 +0100

    LoginAndRegisterWorking

--- src/main/java/org/thatmovie/Auth/Response/SignUpResponse.java
+++ src/main/java/org/thatmovie/Auth/Response/SignUpResponse.java
@@ -7,4 +7,45 @@ public class SignUpResponse {
     private String name;
     private String surname;
     private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/org/thatmovie/doc/WebConfig.java
+++ src/main/java/org/thatmovie/doc/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8100")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
